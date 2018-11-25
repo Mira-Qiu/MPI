80cc899d827cba6463a75e21b1cc58ae5d2cffe7
Mira Qiu <qmm0607@gmail.com>
2018-11-24 23:30:52 -0500

    Update MPI_Demo1.c
    We would like to write a program to create: n processes with the ranks: 0, 1, 2, 3,…(n-1). The processes with the rank 1, 2, 3, ….(n-1) sends a message to the process with the rank: 0. This process receives all of these messages and displays them on the screen. This process also prints a message for itself. In other words:
    
    If I am process1, process2, … or process n-1
                Send my message to process0.
    else if I am process0
                Print my message.
                Receive messages from other processes and print them.

--- MPI_Demo1.c
+++ MPI_Demo1.c
@@ -11,8 +11,9 @@ int main(void)
    int        my_rank;
    int q;
 
-   MPI_Init(NULL, NULL); 
-   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
+   MPI_Init(NULL, NULL);                        // like initialize():(signature, first line of the definition of a function)
+                                                // format of: int MPI_Init(int *argc, char **argv)
+   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);     
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
 
    if (my_rank != 0) 
